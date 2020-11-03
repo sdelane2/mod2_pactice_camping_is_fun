948303c40824c9eca12b65b3bac5236fca3152ac
sdelane2 <seanmartindelaney@gmail.com>
2020-11-03 11:40:43 -0500

    camping

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -1,2 +1,16 @@
 class Camper < ApplicationRecord
+    has_many :signups
+    has_many :activities, through: :signups
+
+    def activities
+        self.signups.map do |signup|
+            activity_array = signup.activity.name
+            activity_array
+        end
+    end
+
+    validates :name, uniqueness: true 
+    validates :age, inclusion: 8..18
+
+
 end
